467395d27065878906af0be8a1236b6360dba033
talesa365 <talesahowell7@gmail.com>
2022-12-21 19:33:02 -0500

    added to url for my app

--- hello_app/hello_site_app/urls.py
+++ hello_app/hello_site_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from hello_site_app.views import HelloWorldView, HelloView
 
 urlpatterns = [
-   path('', view = HelloWorldView.as_view()),
+   path('', hello, view = HelloWorldView.as_view()),
    path('<str:name>', view = HelloView.as_view())
 ]
--- hello_app/hello_site_app/views.py
+++ hello_app/hello_site_app/views.py
@@ -4,8 +4,10 @@ from django.http import HttpResponse
 # Create your views here
 class HelloWorldView(View):
     def get(self, request):
-        return render(request, template_name = 'Hello_world.html')
-    
+        return render(
+            request, 
+            template_name = 'Hello_world.html')
+            
 class HelloView(View):
     def get(self, request, name):
         return render(
